34f06f1b745d640cd7e39ad46d6d8588eaff320b
Aakarsh Sinha <aakarshsak@gmail.com>
2019-09-11 21:30:50 +0530

    Start to finish complete app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.teal[600],
+        ),
+        body: Balls(),
       ),
-    );
+    ),
+  );
+}
+
+class Balls extends StatefulWidget {
+  @override
+  _BallsState createState() => _BallsState();
+}
+
+class _BallsState extends State<Balls> {
+  int ballValue = 1;
+
+  void setBallValue() {
+    setState(() {
+      ballValue = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.lightGreen[600],
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setBallValue();
+                },
+                child: Image.asset('images/ball$ballValue.png'),
+              ),
+            ),
+          ],
+        ));
+  }
+}
